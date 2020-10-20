fa260e36b0cda8841770ee902aa9d1eac9cb7cfc
Andrey Kalkutin <cosmostar@protonmail.com>
2020-10-20 23:53:58 +0500

    Unit tests added

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='First test (1)')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'First test (1)')
+
+
+class HomePageViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='Second test (2)')
+
+    def test_view_url_exists_at_proper_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
