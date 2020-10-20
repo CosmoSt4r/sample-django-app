078cbd7d7fb69963a8473e3a438212e1d0d1c226
Andrey Kalkutin <cosmostar@protonmail.com>
2020-10-20 19:58:05 +0500

    Class based ListView added

--- DjangoSample/settings.py
+++ DjangoSample/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gidm2&*#zd+mta8i^0f!wu6ux#cui%pq6f9i!mm3%3_kh4z-=^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
