a00c176dc02ae6b23f9b321ba4f0d15d085c48a5
Andrey Kalkutin <cosmostar@protonmail.com>
2020-10-20 16:18:21 +0500

    About page added

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import *
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
