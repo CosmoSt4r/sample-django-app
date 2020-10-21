4e16ac8047bdd0873dc3652674879780944bbdc8
Andrey Kalkutin <cosmostar@protonmail.com>
2020-10-21 11:00:05 +0500

    Homepage added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
