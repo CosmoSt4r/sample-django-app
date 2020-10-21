5be8a19149cf8d5f5fbed5bb562ebc50017d4970
Andrey Kalkutin <cosmostar@protonmail.com>
2020-10-21 11:09:31 +0500

    Post detailed view added

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,12 @@ from .models import Post
 class BlogListView(ListView):
 
     model = Post
+    context_object_name = 'posts_list'
     template_name = 'home.html'
 
 
 class BlogDetailView(DetailView):
 
     model = Post
+    context_object_name = 'post'
     template_name = 'post_details.html'
